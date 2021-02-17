17696e6d1773d38bcdf8191ea9b3abc4b52c2bac
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-02-17 12:40:21 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,18 @@
+import { checkForUrl } from './js/urlChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
+console.log(checkForUrl);
+
+alert("Welcome to my natural process language app")
+console.log("I'm working :)");
+
+export {
+    checkForUrl,
+    handleSubmit
+}
